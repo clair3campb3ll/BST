1e052a6898780626fa0f68638e85ef22d4dc6eb0
Claire Campell <ClaireCampell@MacBook-Pro-104.local>
2023-04-12 11:44:47 +0200

    First commit

---
+++ src/Toktik.java
@@ -0,0 +1,13 @@
+public class Toktik {
+    int x = 5;
+    public static void main(String[] argz){
+        
+
+        Toktik myObj = new Toktik();
+        System.out.println(myObj.x);
+
+
+
+    }
+
+}
