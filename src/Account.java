9315789c7c83df50ac39338bd91f0bcf232f038b
Claire Campbell <claire.18.campbell@gmail.com>
2023-04-16 12:30:53 +0200

    Second commit

---
+++ src/Account.java
@@ -0,0 +1,12 @@
+import java.util.LinkedList;
+
+import java.util.LinkedList;
+
+public class Account implements Comparable<Account>{
+    private String name;
+    private String description;
+    private LinkedList<Post> posts;
+
+    public Account
+
+}
--- src/Toktik.java
+++ src/Toktik.java
@@ -1,13 +1,7 @@
+import java.util.Scanner;
+
 public class Toktik {
-    int x = 5;
-    public static void main(String[] argz){
-        
-
-        Toktik myObj = new Toktik();
-        System.out.println(myObj.x);
-
-
-
-    }
-
+    //private static BinarySearchTree<Account> accounts;
+    
+    
 }
