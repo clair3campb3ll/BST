a661bf9bcf44abf532cbe81741202fdbc539c336
Claire Campbell <claire.18.campbell@gmail.com>
2023-04-18 13:01:23 +0200

    Ninth commit

--- src/Toktik.java
+++ src/Toktik.java
@@ -1,4 +1,7 @@
+import java.io.BufferedReader;
 import java.io.FileNotFoundException;
+import java.io.FileReader;
+import java.io.IOException;
 import java.util.Scanner;
 
 public class Toktik {
@@ -44,7 +47,7 @@ public class Toktik {
                         BinaryTreeNode<Account> node2 = accounts.find(account2);
                         if (node2 == null){
                             accounts.insert(account2);
-                            System.out.println("Successfully created account "+account2.getName());
+                            System.out.println("Successfully created account " + account2.getName());
                         }
                         else {
                             System.out.println("An account with that name already exists!");
@@ -55,15 +58,16 @@ public class Toktik {
                         Account account3 = new Account(input.nextLine());
                         BinaryTreeNode<Account> node3 = accounts.find(account3);
                         accounts.delete(account3, node3);
-                        System.out.println("Successfully deleted account "+account3.getName());
+                        System.out.println("Successfully deleted account " + account3.getName());
                         break;
                     case "5": // display all posts for a single account
                         System.out.println("Enter an account name");
                         Account account4 = new Account(input.nextLine());
                         BinaryTreeNode<Account> node4 = accounts.find(account4);
-                        account4 = node4.data;
-                        account4.getPosts();
-
+                        if (node4 != null){
+                            account4 = node4.data;
+                            account4.getPosts();
+                        }
                         break;
                     case "6": // add a new post for an account
                         System.out.println("Enter an account name");
@@ -76,9 +80,46 @@ public class Toktik {
                         int likes = input.nextInt();
                         //BinaryTreeNode<Account> node4 = accounts.find(account4);
                         Post post = new Post(title, vidName, likes);
-                        account5.addPost(post);
+                        BinaryTreeNode<Account> node5 = accounts.find(account5);
+                        if (node5 != null) {
+                            account5 = node5.data;
+                            account5.addPost(post);
+                        }
                         break;
                     case "7": // load a file of actions from disk and process this
+                        System.out.println("Enter the filename to be loaded from disk");
+                        String filename = input.nextLine();
+                        try {
+                            BufferedReader reader = new BufferedReader(new FileReader(filename));
+                            String line;
+                            while((line = reader.readLine()) != null) {
+                                String[] parts = line.split(" ");
+                                if (parts[0].equals("Create")){
+                                    String n = parts[1];
+                                    String d = line.substring(line.indexOf(name) + name.length() +1);
+                                    Account account6 = new Account(n,d);
+                                    accounts.insert(account6);
+                                } // end if
+                                else if (parts[0].equals("Add")){
+                                    String n = parts[1];
+                                    String vidFileName = parts[2];
+                                    String numLikes = parts[3];
+                                    String t = line.substring(line.indexOf(numLikes)+ numLikes.length()+1);
+                                    Account account6 = new Account(n);
+                                    Post p = new Post(t, vidFileName, Integer.parseInt(numLikes));
+                                    BinaryTreeNode<Account> node6 = accounts.find(account6);
+                                    account6 = node6.data;
+                                    if (account6 != null) {
+                                        account6.addPost(p);
+                                    } // end if
+                                } // end else
+
+                            } // end while
+                            reader.close();
+                        } // end try 
+                        catch (IOException e) {
+                            System.out.println("Error while reading file");
+                        } // end catch
                         break;
                     case "8": // quit
                         leave = true;
